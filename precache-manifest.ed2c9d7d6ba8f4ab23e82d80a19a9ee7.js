1335a5ce314c5bdac4633f440eee6cfa7eebe591
esme rodriguez <rodriguezesme24@gmail.com>
2020-07-23 10:23:25 -0700

    Updates

---
+++ precache-manifest.ed2c9d7d6ba8f4ab23e82d80a19a9ee7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c2d04068cbcfb89fdb4cdfcfce5d98d",
+    "url": "/user--directory-/index.html"
+  },
+  {
+    "revision": "38379ef8f2387f0aa034",
+    "url": "/user--directory-/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c0e8ad057fa5050e1033",
+    "url": "/user--directory-/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "38379ef8f2387f0aa034",
+    "url": "/user--directory-/static/js/2.563b6a6a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/user--directory-/static/js/2.563b6a6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0e8ad057fa5050e1033",
+    "url": "/user--directory-/static/js/main.fcfb4d82.chunk.js"
+  },
+  {
+    "revision": "51a8db6245068a7c6b09",
+    "url": "/user--directory-/static/js/runtime-main.aa22a73f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/user--directory-/static/media/logo.5d5d9eef.svg"
+  }
+]);
